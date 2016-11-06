8572969540c899c5c8c48d0416769fbef58229eb
Myles Woerner <myleswoerner@gmail.com>
2016-11-06 14:56:15 -0600

    add alternate options for even number problem

--- keep_even_numbers.rb
+++ keep_even_numbers.rb
@@ -3,14 +3,39 @@
 numbers = [2, 5, 7, 8, 22, 21, 27, 15, 17, 13, 14]
 even_numbers = []
 
-numbers.each do |number|
-  if number.even? == true
-    even_numbers << number
-  end
-end
+# numbers.each do |number|
+#   if number.even? == true
+#     even_numbers << number
+#   end
+# end
 
-p even_numbers
+# p even_numbers
 
-numbers.each do |number|
-  
-end
+# numbers.each do |number|
+
+# end
+
+numbers.each {|number| even_numbers << number if number.even? == true }
+numbers = even_numbers
+
+p numbers
+
+numbers = numbers.each {|number| number.even? == true}
+
+p numbers 
+
+   # incomplete_tasks = []
+    # @tasks.each do |task|
+    #   if task.complete == false
+    #     incomplete_tasks << task
+    #   end
+    # end
+    # @tasks = incomplete_tasks
+
+    # incomplete_tasks = []
+    # @tasks.each { |task| incomplete_tasks << task if task.complete == false }
+    # @tasks = incomplete_tasks
+
+    # @tasks = @tasks.select { |task| task.complete == false }
+
+    # @tasks = @tasks.select { |task| !task.complete }
